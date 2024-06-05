f844cb1fbf79506671e08d4e7ec3338a08e035ff
Adityaa00 <117570853+Adityaa00@users.noreply.github.com>
2024-06-05 21:52:08 +0800

    Update dtInputMember.java

--- dtInputMember.java
+++ dtInputMember.java
@@ -1,32 +1,29 @@
-
 package gymmember1;
 /**
  *
- * @author Aditya - 2201010512
+ * @author Aditya
  */
 
 import java.util.Scanner;
 
-
 public class dtInputMember {
-    
     private int jRecord = 10;
-     public int aRec = 0;
-    
+    public int aRec = 0;
+
     //Array
     private String[][] dtProduk = new String[jRecord][5];
     Scanner dtIN = new Scanner(System.in);
-    
+
     //constactor
-    public dtInputMember (int nval){
+    public dtInputMember(int nval) {
         this.jRecord = nval;
     }
- 
+
     //StoreData
-    public void InputData(){
+    public void InputData() {
         int idx = aRec;
-        
-        if(idx >= jRecord){
+
+        if (idx >= jRecord) {
             System.out.println("Tempat Penyimpanan Data Sudah Penuh");
             System.out.println("");
             return;
@@ -35,122 +32,230 @@ public class dtInputMember {
         System.out.println("~~~~~~~~~~~~~~~~~~");
         System.out.print("Nama Member : ");
         dtProduk[idx][0] = dtIN.nextLine();
-        
+
         System.out.print("Jenis Member : ");
         dtProduk[idx][1] = dtIN.nextLine();
-        
+
         System.out.print("Jenis Kelamin : ");
         dtProduk[idx][2] = dtIN.nextLine();
-        
+
         System.out.print("Tanggal Pendaftaran : ");
         dtProduk[idx][3] = dtIN.nextLine();
-        
+
         System.out.print("Tanggal Habis Member : ");
         dtProduk[idx][4] = dtIN.nextLine();
-        
+
         aRec++;
     }
-    
+
+    //Overloading InputData dengan parameter String memberId
+    public void InputData(String memberId) {
+        int idx = aRec;
+
+        if (idx >= jRecord) {
+            System.out.println("Tempat Penyimpanan Data Sudah Penuh");
+            System.out.println("");
+            return;
+        }
+        System.out.println("Tambah Member Gym dengan ID " + memberId);
+        System.out.println("~~~~~~~~~~~~~~~~~~");
+        System.out.print("Nama Member : ");
+        dtProduk[idx][0] = dtIN.nextLine();
+
+        System.out.print("Jenis Member : ");
+        dtProduk[idx][1] = dtIN.nextLine();
+
+        System.out.print("Jenis Kelamin : ");
+        dtProduk[idx][2] = dtIN.nextLine();
+
+        System.out.print("Tanggal Pendaftaran : ");
+        dtProduk[idx][3] = dtIN.nextLine();
+
+        System.out.print("Tanggal Habis Member : ");
+        dtProduk[idx][4] = dtIN.nextLine();
+
+        aRec++;
+    }
+
     //UpdateData = Mengubah data
-    public void UpdateData(){
+    public void UpdateData() {
         System.out.println("Ubah Data Member Gym");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~");
         System.out.print("Nomor Record: ");
-        
+
         String txidx = dtIN.nextLine();
         int idx = Integer.parseInt(txidx);
         System.out.println("Data Lama");
         System.out.println("~~~~~~~~~");
-        System.out.println("Nama Member: "+dtProduk[idx][0]);
+        System.out.println("Nama Member: " + dtProduk[idx][0]);
         System.out.print("Diubah menjadi: ");
         dtProduk[idx][0] = dtIN.nextLine();
-        
-        System.out.println("Jenis Member: "+dtProduk[idx][1]);
+
+        System.out.println("Jenis Member: " + dtProduk[idx][1]);
         System.out.print("Diubah menjadi: ");
         dtProduk[idx][1] = dtIN.nextLine();
-        
-        System.out.println("Jenis Kelamin: "+dtProduk[idx][2]);
+
+        System.out.println("Jenis Kelamin: " + dtProduk[idx][2]);
         System.out.print("Diubah menjadi: ");
         dtProduk[idx][2] = dtIN.nextLine();
-        
-        System.out.println("Tanggal Pendaftaran: "+dtProduk[idx][3]);
+
+        System.out.println("Tanggal Pendaftaran: " + dtProduk[idx][3]);
         System.out.print("Diubah menjadi: ");
         dtProduk[idx][3] = dtIN.nextLine();
-        
-        System.out.println("Tanggal Habis Member: "+dtProduk[idx][3]);
+
+        System.out.println("Tanggal Habis Member: " + dtProduk[idx][4]);
         System.out.print("Diubah menjadi: ");
         dtProduk[idx][4] = dtIN.nextLine();
-        
-        System.out.println("Data Record ke "+idx+" telah diubah");
-        
-        
-      
-                
+
+        System.out.println("Data Record ke " + idx + " telah diubah");
     }
-    
-    
-    
-    
-            
+
+    //Overloading UpdateData dengan parameter int memberId
+    public void UpdateData(int memberId) {
+        System.out.println("Ubah Data Member Gym dengan ID " + memberId);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+        System.out.print("Nomor Record: ");
+
+        String txidx = dtIN.nextLine();
+        int idx = Integer.parseInt(txidx);
+        System.out.println("Data Lama");
+        System.out.println("~~~~~~~~~");
+        System.out.println("Nama Member: " + dtProduk[idx][0]);
+        System.out.print("Diubah menjadi: ");
+        dtProduk[idx][0] = dtIN.nextLine();
+
+        System.out.println("Jenis Member: " + dtProduk[idx][1]);
+        System.out.print("Diubah menjadi: ");
+        dtProduk[idx][1] = dtIN.nextLine();
+
+        System.out.println("Jenis Kelamin: " + dtProduk[idx][2]);
+        System.out.print("Diubah menjadi: ");
+        dtProduk[idx][22] = "Diubah menjadi: ";
+        dtProduk[idx][2] = dtIN.nextLine();
+
+        System.out.println("Tanggal Pendaftaran: " + dtProduk[idx][3]);
+        System.out.print("Diubah menjadi: ");
+        dtProduk[idx][3] = dtIN.nextLine();
+
+        System.out.println("Tanggal Habis Member: " + dtProduk[idx][4]);
+        System.out.print("Diubah menjadi: ");
+        dtProduk[idx][4] = dtIN.nextLine();
+
+        System.out.println("Data Record ke " + idx + " telah diubah");
+    }
+
     //Destroy = Menghapus Data
-    public void DestroyData(){
+    public void DestroyData() {
         String[][] tmp = new String[jRecord][5];
-        
+
         System.out.println("Hapus Member Gym ");
         System.out.println("~~~~~~~~~~~~~~~~~");
         System.out.println("No Record: ");
-        
+
         String txidx = dtIN.nextLine();
-        
+
         int idx = Integer.parseInt(txidx);
         System.out.println("Data yang dihapus");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("Nama member: "+dtProduk[idx][0]);
-        System.out.println("Jenis Member: "+dtProduk[idx][1]);
-        System.out.println("Jenis Kelamin: "+dtProduk[idx][2]);
-        System.out.println("Tanggal Pendaftaran: "+dtProduk[idx][3]);
-        System.out.println("Tanggal Habis Member: "+dtProduk[idx][4]);
+        System.out.println("Nama member: " + dtProduk[idx][0]);
+        System.out.println("Jenis Member: " + dtProduk[idx][1]);
+        System.out.println("Jenis Kelamin: " + dtProduk[idx][2]);
+        System.out.println("Tanggal Pendaftaran: " + dtProduk[idx][3]);
+        System.out.println("Tanggal Habis Member: " + dtProduk[idx][4]);
 
-        
         int cidx = 0;
-        for(int i=0;i<aRec;i++){
-            if(i == idx){
+        for (int i = 0; i < aRec; i++) {
+            if (i == idx) {
                 continue;
             }
-                
-            tmp[cidx][0] = dtProduk[i][0];   
-            tmp[cidx][1] = dtProduk[i][1];   
+            tmp[cidx][0] = dtProduk[i][0];
+            tmp[cidx][1] = dtProduk[i][1];
             tmp[cidx][2] = dtProduk[i][2];
-            tmp[cidx][2] = dtProduk[i][3];
-            tmp[cidx][2] = dtProduk[i][4];
+            tmp[cidx][3] = dtProduk[i][3];
+            tmp[cidx][4] = dtProduk[i][4];
             cidx++;
         }
         aRec--;
-        for(int i=0; i<aRec; i++){
-            tmp[cidx][0] = dtProduk[i][0];   
-            tmp[cidx][1] = dtProduk[i][1];   
-            tmp[cidx][2] = dtProduk[i][2];  
-            tmp[cidx][2] = dtProduk[i][3];
-            tmp[cidx][2] = dtProduk[i][4];
+        for (int i = 0; i < aRec; i++) {
+            tmp[cidx][0] = dtProduk[i][0];
+            tmp[cidx][1] = dtProduk[i][1];
+            tmp[cidx][2] = dtProduk[i][2];
+            tmp[cidx][3] = dtProduk[i][3];
+            tmp[cidx][4] = dtProduk[i][4];
         }
 
     }
-    
+
+    //Overloading DestroyData dengan parameter String memberId
+    public void DestroyData(String memberId) {
+        String[][] tmp = new String[jRecord][5];
+
+        System.out.println("Hapus Member Gym dengan ID " + memberId);
+        System.out.println("~~~~~~~~~~~~~~~~~");
+        System.out.println("No Record: ");
+
+        int idx = Integer.parseInt(memberId);
+        System.out.println("Data yang dihapus");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Nama member: " + dtProduk[idx][0]);
+        System.out.println("Jenis Member: " + dtProduk[idx][1]);
+        System.out.println("Jenis Kelamin: " + dtProduk[idx][2]);
+        System.out.println("Tanggal Pendaftaran: " + dtProduk[idx][3]);
+        System.out.println("Tanggal Habis Member: " + dtProduk[idx][4]);
+
+        int cidx = 0;
+        for (int i = 0; i < aRec; i++) {
+            if (i == idx) {
+                continue;
+            }
+            tmp[cidx][0] = dtProduk[i][0];
+            tmp[cidx][1] = dtProduk[i][1];
+            tmp[cidx][2] = dtProduk[i][2];
+            tmp[cidx][3] = dtProduk[i][3];
+            tmp[cidx][4] = dtProduk[i][4];
+            cidx++;
+        }
+        aRec--;
+        for (int i = 0; i < aRec; i++) {
+            tmp[cidx][0] = dtProduk[i][0];
+            tmp[cidx][1] = dtProduk[i][1];
+            tmp[cidx][2] = dtProduk[i][2];
+            tmp[cidx][3] = dtProduk[i][3];
+            tmp[cidx][4] = dtProduk[i][4];
+        }
+
+    }
+
     //ViewData = Melihat Data
-    public void viewData(){
-        System.out.println("Data Member Gym ("+aRec+"/"+jRecord+")");
+    public void viewData() {
+        System.out.println("Data Member Gym (" + aRec + "/" + jRecord + ")");
         System.out.println("~~~~~~~~~~~~~~~~");
-        
-        for (int i=0; i<aRec; i++){
-            System.out.println("Data Record ke"+i);
-            System.out.println("Nama Member :"+dtProduk[i][0]);
-            System.out.println("Jenis Member :"+dtProduk[i][1]);
-            System.out.println("Jenis Kelamin :"+dtProduk[i][2]);
-            System.out.println("Tanggal Pendaftaran :"+dtProduk[i][3]);
-            System.out.println("Tanggal Habis Member :"+dtProduk[i][4]);
+        for (int i = 0; i < aRec; i++) {
+            System.out.println("Data Record ke" + i);
+            System.out.println("Nama Member :" + dtProduk[i][0]);
+            System.out.println("Jenis Member :" + dtProduk[i][1]);
+            System.out.println("Jenis Kelamin :" + dtProduk[i][2]);
+            System.out.println("Tanggal Pendaftaran :" + dtProduk[i][3]);
+            System.out.println("Tanggal Habis Member :" + dtProduk[i][4]);
         }
         System.out.println("");
-        
     }
-    
+
+    //Overloading viewData dengan parameter boolean showDeleted
+    public void viewData(boolean showDeleted) {
+        System.out.println("Data Member Gym (" + aRec + "/" + jRecord + ")");
+        System.out.println("~~~~~~~~~~~~~~~~");
+        for (int i = 0; i < aRec; i++) {
+            System.out.println("Data Record ke" + i);
+            System.out.println("Nama Member :" + dtProduk[i][0]);
+            System.out.println("Jenis Member :" + dtProduk[i][1]);
+            System.out.println("Jenis Kelamin :" + dtProduk[i][2]);
+            System.out.println("Tanggal Pendaftaran :" + dtProduk[i][3]);
+            System.out.println("Tanggal Habis Member :" + dtProduk[i][4]);
+            if (showDeleted) {
+                System.out.println("Data ini telah dihapus");
+            }
+        }
+        System.out.println("");
+    }
 }
